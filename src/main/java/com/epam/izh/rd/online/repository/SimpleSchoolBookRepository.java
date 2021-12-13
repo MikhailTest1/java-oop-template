e710c15d7c8d7642e924aa6caeaaebcb41a597a4
Mikhail Kiselev <Mikhail_Kiselev@epam.com>
2021-12-13 09:31:11 +0400

    Completed

--- src/main/java/com/epam/izh/rd/online/repository/SimpleSchoolBookRepository.java
+++ src/main/java/com/epam/izh/rd/online/repository/SimpleSchoolBookRepository.java
@@ -8,8 +8,15 @@ import java.util.Objects;
 
 public class SimpleSchoolBookRepository implements BookRepository{
 private SchoolBook[] schoolBooks = new SchoolBook[]{};
-
-
+    /**
+     * Метод должен сохранять школьную книгу в массив schoolBooks.
+     * Массив при каждом сохранении должен увеличиваться в размере ровно на 1.
+     * То есть он ровно того размера, сколько сущностей мы в него сохранили.
+     * <p>
+     * Одну и ту же книгу МОЖНО сохранить в массиве несколько раз, проверки на то, что такая книга уже сохранена, делать не нужно.
+     * <p>
+     * Если сохранение прошло успешно, метод должен вернуть true.
+     */
 
     @Override
     public boolean save(SchoolBook book) {
@@ -19,7 +26,12 @@ private SchoolBook[] schoolBooks = new SchoolBook[]{};
         schoolBooks = saveBook;
         return true;
     }
-
+    /**
+     * Метод должен находить в массиве schoolBooks все книги по имени.
+     * <p>
+     * Если книги найдены - метод должен их вернуть.
+     * Если найденных по имени книг нет, должен вернуться пустой массив.
+     */
 
     @Override
     public Book[] findByName(String name) {
@@ -33,7 +45,17 @@ private SchoolBook[] schoolBooks = new SchoolBook[]{};
         }
         return findBook;
     }
-
+    /**
+     * Метод должен удалять книги из массива schoolBooks по названию.
+     * Если книг с одинаковым названием в массиве несколько, метод должен удалить их все.
+     * <p>
+     * Важно: при удалении книги из массива размер массива должен уменьшиться!
+     * То есть, если мы сохранили 2 разные книги и вызвали count() (метод ниже), то он должен вернуть 2.
+     * Если после этого мы удалили 1 книгу, метод count() должен вернуть 1.
+     * <p>
+     * Если хотя бы одна книга была найдена и удалена, метод должен вернуть true, в противном случае,
+     * если книга не была найдена, метод должен вернуть false.
+     */
     @Override
     public boolean removeByName(String name) {
 
@@ -53,7 +75,9 @@ private SchoolBook[] schoolBooks = new SchoolBook[]{};
             return true;
         }
     }
-
+    /**
+     * Метод возвращает количество сохраненных сущностей в массиве schoolBooks.
+     */
     @Override
     public int count() {
         return schoolBooks.length;
--- src/main/java/com/epam/izh/rd/online/service/SimpleSchoolBookService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleSchoolBookService.java
@@ -18,6 +18,18 @@ public class SimpleSchoolBookService implements  BookService {
 
 
     }
+    /**
+     * Метод должен сохранять книгу.
+     *
+     * Перед сохранением книги нужно проверить, сохранен ли такой автор в базе авторов.
+     * То есть вы должен взять имя и фамилию автора из книги и обратиться к сервису авторов и узнать о наличии такого автора.
+     * Напомню, что мы считаем, что двух авторов с одинаковыми именем и фамилией быть не может.
+     *
+     * Если такой автор сущесвует (сохранен) - значит можно сохранять и книгу.
+     * Если же такого автора в базе нет, значит книгу сохранять нельзя.
+     *
+     * Соответственно, если книга была успешно сохранена - метод возвращает true, если же книга не была сохранена - метод возвращает false.
+     */
     @Override
     public boolean save(Book book) {
         SchoolBook diffBook = ((SchoolBook) book);
@@ -27,35 +39,55 @@ public class SimpleSchoolBookService implements  BookService {
         }
         return false;
     }
-
+    /**
+     * Метод должен находить книгу по имени.
+     * <p>
+     * По факту, он просто обращается к репозиторию с книгами и вызывает аналогичный метод, псоле чего возвращает результат.
+     */
     @Override
     public Book[] findByName(String name) {
         return schoolBookBookRepository.findByName(name);
     }
 
-
-
+    /**
+     * Метод должен находить количество сохраненных книг по конкретному имени книги.
+     */
     @Override
     public int getNumberOfBooksByName(String name) {
         return schoolBookBookRepository.findByName(name).length;
     }
-
+    /**
+     * Метод должен удалять все книги по имени.
+     * <p>
+     * По факту, он просто обращается к репозиторию с книгами и вызывает аналогичный метод, псоле чего возвращает результат.
+     */
     @Override
     public boolean removeByName(String name) {
         return schoolBookBookRepository.removeByName(name);
     }
-
-
+    /**
+     * Метод должен возвращать количество всех книг.
+     * <p>
+     * По факту, он просто обращается к репозиторию с книгами и вызывает аналогичный метод, псоле чего возвращает результат.
+     */
     @Override
     public int count() {
         return schoolBookBookRepository.count();
     }
-
+    /**
+     * Метод должен возвращать автора книги по названию книги.
+     *
+     * То есть приждется сходить и в репозиторий с книгами и в сервис авторов.
+     *
+     * Если такой книги не найдено, метод должен вернуть null.
+     */
     @Override
     public Author findAuthorByBookName(String name) {
 
-        if (authorService.findByFullName(schoolBookBookRepository.findByName(name)[0].getAuthorName(), schoolBookBookRepository.findByName(name)[0].getAuthorLastName()) != null) {
-            return authorService.findByFullName(schoolBookBookRepository.findByName(name)[0].getAuthorName(), schoolBookBookRepository.findByName(name)[0].getAuthorLastName());
+        if (authorService.findByFullName(schoolBookBookRepository.findByName(name)[0].getAuthorName(),
+                schoolBookBookRepository.findByName(name)[0].getAuthorLastName()) != null) {
+            return authorService.findByFullName(schoolBookBookRepository.findByName(name)[0].getAuthorName(),
+                    schoolBookBookRepository.findByName(name)[0].getAuthorLastName());
         } else
             return null;
     }
